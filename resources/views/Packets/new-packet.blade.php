04b4a6f1ba11099190775c8c86885ab0a4cd61ac
Gabriel Pereira <gabrielphp20@gmail.com>
2025-06-10 14:14:04 -0300

    Criada a verificação do formulário antes do envio 1/2.

--- resources/views/Packets/new-packet.blade.php
+++ resources/views/Packets/new-packet.blade.php
@@ -35,18 +35,18 @@
                                 <div class="row pb-2">
                                     <div class="col-4">
                                         <span class="font-weight-bold modal-label">Recebedor:</span>
-                                        <input type="text" name="receiver" class="form-control input-format mt-2">
+                                        <input type="text" name="receiver" class="form-control input-format mt-2" id="receiver">
                                     </div>
 
                                     <div class="col-4">
                                         <span class="font-weight-bold modal-label">Destinatário:</span>
-                                        <input type="text" name="recipient" class="form-control input-format mt-2">
+                                        <input type="text" name="recipient" class="form-control input-format mt-2" id="recipient">
                                     </div>
 
                                     <div class="col-4">
                                         <span class="font-weight-bold modal-label">Unidade:</span>
-                                        <select name="unit" class="form-control input-format mt-1">
-                                            <option selected disabled>Selecione</option>
+                                        <select name="unit" class="form-control input-format mt-1" id="unit">
+                                            <option value="selecione" selected disabled>Selecione</option>
                                             @foreach ($units as $unit)
                                                 <option value="{{ $unit->number }} - BL 0{{ $unit->block }}">{{ $unit->number }} - BL 0{{ $unit->block }}</option>
                                             @endforeach
@@ -128,10 +128,41 @@
             const text = button.querySelector('.button-text');
             const spinner = button.querySelector('.spinner-border');
             const form = document.getElementById('new-packet');
+            const receiver = document.getElementById('receiver').value;
+            const recipient = document.getElementById('recipient').value;
+            const unit = document.getElementById('unit').value;
 
-            text.classList.add('d-none');
-            spinner.classList.remove('d-none');
-            form.submit();
+            function showToast(message){
+                const notyf = new Notyf({
+                    position: {
+                        x: 'right',
+                        y: 'top',
+                    }
+                });
+
+                notyf
+                    .error({
+                        message: message,
+                        dismissible: true,
+                        duration: 7000
+                    })
+            }
+
+
+            if (receiver == ''){
+                const message = 'Preencha corretamente o nome do recebedor';
+                showToast(message);
+            }else if(recipient == ''){
+                const message = 'Preencha corretamente o nome do destinatário';
+                showToast(message);
+            }else if(unit == 'selecione'){
+                const message = 'Selecione a unidade do destinatário';
+                showToast(message);
+            }else{
+                text.classList.add('d-none');
+                spinner.classList.remove('d-none');
+                form.submit();
+            }
         });
     </script>
 @endsection
