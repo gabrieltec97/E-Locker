2625aaeb55b4e7abc600502300243fac49134fd5
Gabriel Pereira <gabrielphp20@gmail.com>
2025-06-18 13:49:28 -0300

    Inserido icone correto de navegador na tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,8 @@
     <title>Login - E-Locker entregas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+    <link sizes="76x76" href="../assets/img/e-locker.png">
+    <link rel="icon" type="image/png" href="../assets/img/e-locker.png">
 </head>
 <body>
 
